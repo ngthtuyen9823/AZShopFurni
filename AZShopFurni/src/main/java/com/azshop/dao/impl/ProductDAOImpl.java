3c288450bbb01e306184a489ea0c68af8e7358dd
Nguyen Thi Thanh Tuyen <camuccon98@gmail.com>
2023-10-28 20:41:35 +0700

    AZS-06 | Show product details

--- AZShopFurni/src/main/java/com/azshop/dao/impl/ProductDAOImpl.java
+++ AZShopFurni/src/main/java/com/azshop/dao/impl/ProductDAOImpl.java
@@ -8,18 +8,25 @@ import java.util.List;
 
 import com.azshop.connection.DBConnection;
 import com.azshop.dao.ICategoryDAO;
+import com.azshop.dao.IItemDAO;
+import com.azshop.dao.IItemImageDAO;
 import com.azshop.dao.IProductDAO;
 import com.azshop.models.CategoryModel;
+import com.azshop.models.ItemImageModel;
+import com.azshop.models.ItemModel;
 import com.azshop.models.ProductModel;
 
 public class ProductDAOImpl implements IProductDAO {
 	ICategoryDAO cateDAO = new CategoryDAOImpl();
+	IItemDAO itemDAO = new ItemDAOImpl();
+	IItemImageDAO itemImageDAO = new ItemImageDAOImpl();
 	Connection conn = null;
 
 	@Override
 	public List<ProductModel> findAll() {
 		String sql = "Select * from PRODUCT";
 		List<ProductModel> list = new ArrayList<ProductModel>();
+
 		try {
 			new DBConnection();
 			conn = DBConnection.getConnection();
@@ -43,6 +50,7 @@ public class ProductDAOImpl implements IProductDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		return list;
 	}
 
@@ -50,6 +58,7 @@ public class ProductDAOImpl implements IProductDAO {
 	public List<ProductModel> findByCategoryID(int cateId) {
 		String sql = "Select * from PRODUCT where CategoryID=?";
 		List<ProductModel> list = new ArrayList<ProductModel>();
+		
 		try {
 			new DBConnection();
 			conn = DBConnection.getConnection();
@@ -57,16 +66,19 @@ public class ProductDAOImpl implements IProductDAO {
 			ps.setInt(1, cateId);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				CategoryModel cateModel = cateDAO.findOne(rs.getInt("CategoryID"));
 				ProductModel model = new ProductModel();
-
-				model.setProductID(rs.getInt("ProductID"));
+				int productID = rs.getInt("ProductID");
+				
+				model.setProductID(productID);
 				model.setProductName(rs.getString("ProductName"));
 				model.setDescription(rs.getString("Description"));
 				model.setOrigin(rs.getString("Origin"));
 				model.setSupplierID(rs.getInt("SupplierID"));
-				model.setCategoryID(cateModel.getCategoryID());
+				model.setCategoryID(rs.getInt("CategoryID"));
 				model.setMaterial(rs.getString("Material"));
+				model.setDisplayedImage(itemImageDAO.findByProductID(productID).get(0).getImage());
+				model.setDisplayedPromotionPrice(itemDAO.findDisplayedPromotionPrice(productID));
+				model.setDisplayedOriginalPrice(itemDAO.findDisplayedOriginalPrice(productID));
 
 				list.add(model);
 			}
@@ -75,6 +87,7 @@ public class ProductDAOImpl implements IProductDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		return list;
 	}
 
@@ -82,6 +95,7 @@ public class ProductDAOImpl implements IProductDAO {
 	public List<ProductModel> findWithCount(int count) {
 		String sql = "Select * from PRODUCT limit ?";
 		List<ProductModel> list = new ArrayList<ProductModel>();
+		
 		try {
 			new DBConnection();
 			conn = DBConnection.getConnection();
@@ -106,13 +120,15 @@ public class ProductDAOImpl implements IProductDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		return list;
 	}
 
 	@Override
 	public ProductModel findOne(int id) {
-		ProductModel model = new ProductModel();
 		String sql = "Select * from PRODUCT where ProductID=?";
+		ProductModel model = new ProductModel();
+		
 		try {
 			new DBConnection();
 			conn = DBConnection.getConnection();
@@ -120,35 +136,46 @@ public class ProductDAOImpl implements IProductDAO {
 			ps.setInt(1, id);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				model.setProductID(rs.getInt("ProductID"));
+				int productID = rs.getInt("ProductID");
+				
+				model.setProductID(productID);
 				model.setProductName(rs.getString("ProductName"));
 				model.setDescription(rs.getString("Description"));
 				model.setOrigin(rs.getString("Origin"));
 				model.setSupplierID(rs.getInt("SupplierID"));
 				model.setCategoryID(rs.getInt("CategoryID"));
 				model.setMaterial(rs.getString("Material"));
+				model.setAvgRating((float) 4.9); // truy xuat trong bang detail (chưa có)
+				model.setNumOfRating(195); // truy xuat trong baang detail (chưa có)
+				model.setSoldTotal(980); // truy xuat trong order (chưa có)
+				model.setDisplayedPromotionPrice(itemDAO.findDisplayedPromotionPrice(productID));
+				model.setDisplayedOriginalPrice(itemDAO.findDisplayedOriginalPrice(productID));
+				model.setListItem(itemDAO.findByProductID(productID));
+				model.setListItemImage(itemImageDAO.findByProductID(productID));
+
 			}
 			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		return model;
 	}
 
 	public static void main(String[] args) {
 		IProductDAO productDAO = new ProductDAOImpl();
-//
+		
 //		List<ProductModel> list = productDAO.findAll();
 //		System.out.println(list);
-//
-//		List<ProductModel> listByCate = productDAO.findByCategoryID(101);
-//		System.out.println(listByCate);
+
+		List<ProductModel> listByCate = productDAO.findByCategoryID(101);
+		System.out.println(listByCate);
 //
 //		List<ProductModel> listWithCount = productDAO.findWithCount(1);
 //		System.out.println(listWithCount);
 
-		ProductModel model = productDAO.findOne(101001);
-		System.out.println(model);
+//		ProductModel model = productDAO.findOne(101004);
+//		System.out.println(model);
 	}
 
 }
--- AZShopFurni/src/main/java/com/azshop/models/ProductModel.java
+++ AZShopFurni/src/main/java/com/azshop/models/ProductModel.java
@@ -1,5 +1,7 @@
 package com.azshop.models;
 
+import java.util.List;
+
 public class ProductModel {
 	private int productID;
 	private String productName;
@@ -8,8 +10,20 @@ public class ProductModel {
 	private int supplierID;
 	private int categoryID;
 	private String material;
+
+	private float avgRating;
+	private int numOfRating;
+	private int soldTotal;
+	private int displayedPromotionPrice;
+	private int displayedOriginalPrice;
+	private List<ItemModel> listItem;
+	private List<ItemImageModel> listItemImage;
+	private String displayedImage;
+
 	public ProductModel(int productID, String productName, String description, String origin, int supplierID,
-			int categoryID, String material) {
+			int categoryID, String material, float avgRating, int numOfRating, int soldTotal,
+			int displayedPromotionPrice, int displayedOriginalPrice, List<ItemModel> listItem,
+			List<ItemImageModel> listItemImage, String displayedImage) {
 		super();
 		this.productID = productID;
 		this.productName = productName;
@@ -18,57 +32,148 @@ public class ProductModel {
 		this.supplierID = supplierID;
 		this.categoryID = categoryID;
 		this.material = material;
+		this.avgRating = avgRating;
+		this.numOfRating = numOfRating;
+		this.soldTotal = soldTotal;
+		this.displayedPromotionPrice = displayedPromotionPrice;
+		this.displayedOriginalPrice = displayedOriginalPrice;
+		this.listItem = listItem;
+		this.listItemImage = listItemImage;
+		this.displayedImage = displayedImage;
 	}
+
 	public ProductModel() {
 		super();
 	}
+
 	public int getProductID() {
 		return productID;
 	}
+
 	public void setProductID(int productID) {
 		this.productID = productID;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getOrigin() {
 		return origin;
 	}
+
 	public void setOrigin(String origin) {
 		this.origin = origin;
 	}
+
 	public int getSupplierID() {
 		return supplierID;
 	}
+
 	public void setSupplierID(int supplierID) {
 		this.supplierID = supplierID;
 	}
+
 	public int getCategoryID() {
 		return categoryID;
 	}
+
 	public void setCategoryID(int categoryID) {
 		this.categoryID = categoryID;
 	}
+
 	public String getMaterial() {
 		return material;
 	}
+
 	public void setMaterial(String material) {
 		this.material = material;
 	}
+
+	public float getAvgRating() {
+		return avgRating;
+	}
+
+	public void setAvgRating(float avgRating) {
+		this.avgRating = avgRating;
+	}
+
+	public int getNumOfRating() {
+		return numOfRating;
+	}
+
+	public void setNumOfRating(int numOfRating) {
+		this.numOfRating = numOfRating;
+	}
+
+	public int getSoldTotal() {
+		return soldTotal;
+	}
+
+	public void setSoldTotal(int soldTotal) {
+		this.soldTotal = soldTotal;
+	}
+
+	public int getDisplayedPromotionPrice() {
+		return displayedPromotionPrice;
+	}
+
+	public void setDisplayedPromotionPrice(int displayedPromotionPrice) {
+		this.displayedPromotionPrice = displayedPromotionPrice;
+	}
+
+	public int getDisplayedOriginalPrice() {
+		return displayedOriginalPrice;
+	}
+
+	public void setDisplayedOriginalPrice(int displayedOriginalPrice) {
+		this.displayedOriginalPrice = displayedOriginalPrice;
+	}
+
+	public List<ItemModel> getListItem() {
+		return listItem;
+	}
+
+	public void setListItem(List<ItemModel> listItem) {
+		this.listItem = listItem;
+	}
+
+	public List<ItemImageModel> getListItemImage() {
+		return listItemImage;
+	}
+
+	public void setListItemImage(List<ItemImageModel> listItemImage) {
+		this.listItemImage = listItemImage;
+	}
+
 	@Override
 	public String toString() {
 		return "ProductModel [productID=" + productID + ", productName=" + productName + ", description=" + description
 				+ ", origin=" + origin + ", supplierID=" + supplierID + ", categoryID=" + categoryID + ", material="
-				+ material + "]";
+				+ material + ", avgRating=" + avgRating + ", numOfRating=" + numOfRating + ", soldTotal=" + soldTotal
+				+ ", displayedPromotionPrice=" + displayedPromotionPrice + ", displayedOriginalPrice="
+				+ displayedOriginalPrice + ", listItem=" + listItem + ", listItemImage=" + listItemImage
+				+ ", displayedImage=" + displayedImage + "]";
 	}
-	
+
+	public String getDisplayedImage() {
+		return displayedImage;
+	}
+
+	public void setDisplayedImage(String displayedImage) {
+		this.displayedImage = displayedImage;
+	}
+
 }
