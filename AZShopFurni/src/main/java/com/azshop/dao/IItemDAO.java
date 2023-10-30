bc6aa08872a4ff70dd0f25c2648a26a921207862
NguyênNguyên <billbatri085@gmail.com>
2023-10-30 19:29:13 +0700

    Product, Item

--- AZShopFurni/src/main/java/com/azshop/dao/IItemDAO.java
+++ AZShopFurni/src/main/java/com/azshop/dao/IItemDAO.java
@@ -3,9 +3,13 @@ package com.azshop.dao;
 import java.util.List;
 
 import com.azshop.models.ItemModel;
+import com.azshop.models.ProductModel;
 
 public interface IItemDAO {
 	List<ItemModel> findAll();
 	List<ItemModel> findByProductID(int productID);
 	ItemModel findOne(int id);
+	void insertItem (ItemModel model);
+	void deleteItem (int ItemId);
+	void updateItem (ItemModel model);
 }
