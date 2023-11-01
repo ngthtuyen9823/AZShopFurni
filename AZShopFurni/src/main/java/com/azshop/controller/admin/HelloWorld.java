35c3fd365d03b8d3c2b81a96c903b862266c2424
ladat <ladat01626362989@gmail.com>
2023-11-01 21:20:05 +0700

    AZS-10 | update file jsp Account, Customer of Admin

--- AZShopFurni/src/main/java/com/azshop/controller/admin/HelloWorld.java
+++ AZShopFurni/src/main/java/com/azshop/controller/admin/HelloWorld.java
@@ -15,7 +15,7 @@ import com.azshop.models.AccountModel;
 import com.azshop.service.IAccountService;
 import com.azshop.service.impl.AccountServiceImpl;
 
-@WebServlet(urlPatterns = { "/adminHome" })
+@WebServlet(urlPatterns = {  })
 public class HelloWorld extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
